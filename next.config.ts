679031339693a2d075efcd5a92a00b2294384417
chandraanggaradiputra <anggarasixteen@gmail.com>
2026-08-23 22:22:59 +0700

    Revisi Next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,61 +1,30 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "app.maschandigital.id",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "maschandigital.id",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "images.unsplash.com",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "*.gravatar.com",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "secure.gravatar.com",
-//       },
-//     ],
-//   },
-// };
-
-// export default nextConfig;
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "app.maschandigital.id",
-      },
-      {
-        protocol: "https",
-        hostname: "maschandigital.id",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "secure.gravatar.com",
-      },
-      {
-        protocol: "https",
-        hostname: "*.gravatar.com",
-      },
+      { protocol: "https", hostname: "app.maschandigital.id" },
+      { protocol: "https", hostname: "maschandigital.id" },
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "*.gravatar.com" },
+      { protocol: "https", hostname: "secure.gravatar.com" },
     ],
   },
+  // Header Keamanan Tambahan
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/(.*)",
+  //       headers: [
+  //         { key: "X-Content-Type-Options", value: "nosniff" },
+  //         { key: "X-Frame-Options", value: "DENY" },
+  //         { key: "X-XSS-Protection", value: "1; mode=block" },
+  //         { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
